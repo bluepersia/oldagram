e8b7a2e3a7638800d4f65a6489e7deb318b1fcd0
Marco Enrique <fuzzyslash@hotmail.com>
2026-07-31 16:43:04 -0600

    Add main feed

---
+++ src/scripts/types/PostData.ts
@@ -0,0 +1,12 @@
+type PostData = {
+  id: number;
+  name: string;
+  username: string;
+  location: string;
+  avatar: string;
+  post: string;
+  comment: string;
+  likes: number;
+};
+
+export type { PostData };
